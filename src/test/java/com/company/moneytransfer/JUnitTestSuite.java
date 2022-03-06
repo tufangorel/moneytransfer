aa00ff353702dbd0e2f7763cf606f42c8fe66a87
tufangorel <tufang@gmail.com>
2022-03-06 15:21:20 +0300

    use logger in test class.

--- src/test/java/com/company/moneytransfer/JUnitTestSuite.java
+++ src/test/java/com/company/moneytransfer/JUnitTestSuite.java
@@ -4,10 +4,13 @@ import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ExtendWith(VertxExtension.class)
 public class JUnitTestSuite {
 
+    private static final Logger logger = LoggerFactory.getLogger(JUnitTestSuite.class);
 
     @Test
     public void testSomething(VertxTestContext context) {
